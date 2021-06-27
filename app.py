c2209b11a3695bb2232ab4842f7e9fa4efa15e7c
Fausto De La Rosa Mañón <56518301+fmanon@users.noreply.github.com>
2021-06-26 23:41:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-# from flask_pymongo import PyMongo
+from flask import redirect
+from flask_pymongo import PyMongo
 
 
 # -- Initialization section --
@@ -16,31 +17,52 @@ events = [
     ]
 
 # name of database
-# app.config['MONGO_DBNAME'] = 'database-name'
+app.config['MONGO_DBNAME'] = 'database'
 
 # URI of database
-# app.config['MONGO_URI'] = 'mongo-uri'
+app.config['MONGO_URI'] = 'mongodb+srv://admin:nFqmtMPS2ndjg466@cluster0.qkcvq.mongodb.net/database?retryWrites=true&w=majority'
 
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 # -- Routes section --
 # INDEX
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET','POST'])
 
 def index():
-    return render_template('index.html', events = events)
+    if request.method=="POST":
+        return render_template('index.html')
+    else:
+        return redirect('/add')
 
 
 # CONNECT TO DB, ADD DATA
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 
 def add():
-    # connect to the database
+    if request.method == 'GET':
+        return render_template('add.html')
+    else:
+        item_date = request.form['date']
+        item_name = request.form['name']
+        item_price = request.form['price']
+        item_bar_code = request.form['bar_code']
 
-    # insert new data
+        # connect to the database
+        events = mongo.db.events
+        # insert new data
+        events.insert({'date':item_date, 'item':item_name, 'price':item_price, 'bar_code':item_bar_code})
+        # return a message to the user
+        return redirect('/inventory')
 
-    # return a message to the user
-    return ""
+
+
+@app.route('/inventory')
+
+def inventory():
+    collection = mongo.db.events
+    events = collection.find({}).sort('date', 1)
+
+    return render_template('inventory.html', events = events)
